196f20bb69de0d2deea4acb44df1ae88dee7626a
daehyun <kimdeahyeon5445@gmail.com>
2023-11-06 07:52:56 +0900

    버블 차트 구현 중

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -5,6 +5,7 @@ import {usePageStore} from "../store";
 import React, {useCallback, useEffect, useState} from "react";
 import Paging from "../components/page/Paging";
 import {useLocation} from "react-router-dom";
+import BubbleChart from "../components/bubble-chart/BubbleChart";
 
 const dummyTopic:Topic = {
     title: "서버로부터 응답이 없습니다",
@@ -45,6 +46,7 @@ export default function TopicPage() {
     if (isLoading || isError) {
         return(
             <div className="container">
+                <BubbleChart/>
                 <div className="category_name">{detail.name}</div>
                 {TopicCard(dummyTopic)}
             </div>
