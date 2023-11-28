8fd86ee4fdd35d6c744192b8b7fdc5580afb6fc7
daehyun <kimdeahyeon5445@gmail.com>
2023-11-29 02:25:37 +0900

    chat scss 시작

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -7,13 +7,13 @@ interface MessagesProp {
 export default function Messages(prop:MessagesProp) {
     let messages:Array<Message> = prop.messages
     return(
-        <div>
+        <div className='messages_container'>
             {messages.map((m) => {
                 return (
-                    <div key={m.id}>
-                    <div>{m.username}</div>
-                    <div>{m.message}</div>
-                </div>
+                    <div className="message_container" key={m.id}>
+                        <div className="msg_username">{m.username}</div>
+                        <div className="msg_text">{m.message}</div>
+                    </div>
                 )
             })}
         </div>
