a6ed3d203972c818ad68fd6879ba7aa5e8a8fd1c
daehyun <kimdeahyeon5445@gmail.com>
2023-12-05 22:21:11 +0900

    카테고리 Link위치 수정

--- src/components/header/HeaderBottom.tsx
+++ src/components/header/HeaderBottom.tsx
@@ -23,10 +23,10 @@ export default function HeaderBottom() {
 function Card(data:Detail) {
     const {cur, setCur} = usePageStore();
     return(
-        <div onClick={() => setCur(data.idx)} className={cur === data.idx ? "header_bottom_card select" : "header_bottom_card"}>
-            <Link to={data.url}>
+        <Link to={data.url}>
+            <div onClick={() => setCur(data.idx)} className={cur === data.idx ? "header_bottom_card select" : "header_bottom_card"}>
                 {data.name}
-            </Link>
-        </div>
+            </div>
+        </Link>
     )
 }
