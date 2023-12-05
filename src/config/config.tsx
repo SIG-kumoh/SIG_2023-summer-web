93a095f6bfe3c90e252dde105f003b04cf9afccf
daehyun <kimdeahyeon5445@gmail.com>
2023-12-06 04:28:27 +0900

    로딩 화면 위치 개선

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -66,14 +66,15 @@ export interface Node {
 export function Loading() {
         return(
             <div className="wrapper">
-            <div className="box-wrap">
-                <div className="box one"></div>
-                <div className="box two"></div>
-                <div className="box three"></div>
-                <div className="box four"></div>
-                <div className="box five"></div>
-                <div className="box six"></div>
+                <div className="box-wrap">
+                    <div className="box one"></div>
+                    <div className="box two"></div>
+                    <div className="box three"></div>
+                    <div className="box four"></div>
+                    <div className="box five"></div>
+                    <div className="box six"></div>
+                </div>
             </div>
-        </div>)
+        )
 
 }
